a83bebb4a4f18f71400e6391c1b9a2d8526dda78
bzhasso019 <144123070+bzhasso019@users.noreply.github.com>
2024-05-30 19:16:25 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
-<?php $db = new PDO('mysql:host=localhost;dbname=u67411', 'u67411', '9275880');
+<?php $db = new PDO('mysql:host=localhost;dbname=u67411', 'u67411', '9275980');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
